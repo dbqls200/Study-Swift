c0e597bd463fbd3a66fb3f163810c94a7c6deee0
YuBeen <dbqls200@gmail.com>
2022-03-18 14:36:14 +0900

    New Project
    새로운 프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김유빈 on 2022/03/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

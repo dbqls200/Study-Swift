e60f87264b96dd646bb41d5d8ad1c0908d1cad67
YuBeen <87077859+dbqls200@users.noreply.github.com>
2022-03-21 16:25:58 +0900

    study 9_Protocols
    chap 9 프로토콜 실습

--- 100DaysofSwift/9_Protocols.playground/Contents.swift
+++ 100DaysofSwift/9_Protocols.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 // Protocols
 
 // Protocols
-
+/*
 protocol Identifiable {
     var id: String { get set }
 }
@@ -15,3 +15,80 @@ struct User: Identifiable {
 func displayID(thing: Identifiable) {
     print("My ID is \(thing.id)")
 }
+*/
+
+
+// Protocol inheritance
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+
+
+// Extension
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+let number = 8
+number.squared()
+
+number.isEven
+
+
+
+// Protocol extensions
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+
+
+// Protocol-oriented Programming
+
+protocol Identifiable {
+    var id: String { get set }
+    func identify()
+}
+
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id)")
+    }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+let twostraws = User(id: "twostraws")
+twostraws.identify()

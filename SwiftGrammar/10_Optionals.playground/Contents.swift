12eb74d3d745be969bc4c863d057e7eb199007ef
YuBeen <87077859+dbqls200@users.noreply.github.com>
2022-03-24 15:24:51 +0900

    10. optionals 실습코드
    옵셔널 학습 및 실스코드 작성

--- SwiftGrammar/10_Optionals.playground/Contents.swift
+++ SwiftGrammar/10_Optionals.playground/Contents.swift
@@ -17,6 +17,13 @@ age
 var name: String? = nil
 name = "John"
 
+name!.count
+
+type(of: name!)
+
+type(of: name)
+
+
 if let unwrapped = name {
     print("\(unwrapped.count) letters")
 } else {
@@ -33,4 +40,121 @@ func greet(_ name: String?) {
         return
     }
     print("Hello, \(unwrapped)")
+    
+    unwrapped.count
+}
+
+
+greet("John")
+
+
+
+// Force unwrapping
+
+let str = "5"
+
+let num = Int(str)
+type(of: num)
+
+let number = Int(str)!
+type(of: number)
+
+
+
+// Implicitly unwrapped optionals
+
+var young: Int! = nil
+
+young = 20
+type(of: young)
+young += 1
+young
+
+var old: Int? = nil
+
+old = 80
+type(of: old)
+// old += 1
+
+
+
+// Nil coalescing
+
+func username(for id: Int) -> String? {
+    if id == 1 {
+        return "Taylor Swift"
+    } else {
+        return nil
+    }
+}
+
+let user = username(for: 25) ?? "Anonymous"
+
+
+
+// Optional chaining
+
+let names = ["John", "Paul", "George", "Ringo"]
+
+let beatles = names.first?.uppercased()
+
+
+
+// Optional try
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+    
+
+    do {
+        try checkPassword("password")
+        print("That password is good!")
+    } catch {
+        print("You can't use that password")
+    }
+}
+
+
+
+// Failable initializers
+
+let line = "5"
+let numb = Int(line)
+
+struct Person {
+    var id: String
+    init?(id: String) {
+        if id.count == 9 {
+            self.id == id
+        } else {
+            return nil
+        }
+    }
+}
+
+
+
+// Typecasting
+
+class Animal()
+class Fish: Animal()
+class Dog: Animal() {
+    func makeNoise() {
+        print("woof!")
+    }
+}
+
+let pets = [Fish(), Dog(), Fish(), Dog()]
+
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
 }
